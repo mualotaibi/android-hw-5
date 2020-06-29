d99b2b1d604fd2e0b8d8e20818c2265cdedb5526
munera <muneraalotaibii@gmail.com>
2020-06-29 23:04:36 +0300

    done hw-5

---
+++ MyApp/app/src/main/java/com/example/myapp/Activity3.java
@@ -0,0 +1,16 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
